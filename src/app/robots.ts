2308cf80130c945fa5bf391479629844c9018f18
shareef6907 <shareef6907@users.noreply.github.com>
2026-07-16 20:12:44 +0300

    Allow crawling in robots.txt; meta noindex still blocks indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,8 @@
 import { MetadataRoute } from 'next'
-import { brand } from '@/config/brand'
-
 export default function robots(): MetadataRoute.Robots {
-  const indexable = brand.siteIndexable
   return {
-    rules: indexable
-      ? { userAgent: '*', allow: '/' }
-      : { userAgent: '*', disallow: '/' },
-    sitemap: `https://www.premieralive.com/sitemap.xml`,
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://www.premieralive.com/sitemap.xml',
+    host: 'https://www.premieralive.com',
   }
 }
